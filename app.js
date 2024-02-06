dc71e3f9055b9a1abb34f3ad7b3072e3535523fe
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2024-02-07 00:59:25 +0800

    update: set up index.pug

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pub');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -49,4 +49,7 @@ app.use(function(err, req, res, next) {
     res.render('error');
   });
   
-module.exports = app;
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on https://localhost:${port} !`);
+});
