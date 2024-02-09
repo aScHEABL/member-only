fdadbf0016797a60b5ddad9f6ad2ba6b55062fca
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2024-02-09 17:40:59 +0800

    update: password strength function

---
+++ utils/passwordStrength.js
@@ -0,0 +1,10 @@
+exports.testPassword = function (pwString) {
+    let strength = 0;
+
+    strength += /[A-Z]+/.test(pwString) ? 1 : 0;
+    strength += /[a-z]+/.test(pwString) ? 1 : 0;
+    strength += /[0-9]+/.test(pwString) ? 1 : 0;
+    strength += /[\W]+/.test(pwString) ? 1 : 0;
+
+    return strength;
+}
